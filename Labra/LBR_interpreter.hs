7ec1f199d990918cc14fe61271f5f66cd2de971f
DoggyDogWhirl <90369847+DoggyDogWhirl@users.noreply.github.com>
2021-12-10 17:00:21 -0500

    Update LBR_interpreter.hs

--- Labra/LBR_interpreter.hs
+++ Labra/LBR_interpreter.hs
@@ -14,7 +14,7 @@ main = do
   rawCode <- readFile codeFile
   let code = formatCode rawCode
   let results = Array [evaluate Nothing line resultInput results n | (line, n) <- zip code [0..] ]
-  putStrLn "== Labra Interpreter ==\n==(cannot OUTPUT _{})=="
+  putStrLn "== Labra Interpreter ==\n==(cannot OUTPUT A{})=="
   putStrLn . output . last $ (unarray results)
 
 
